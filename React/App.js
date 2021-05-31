cdfdb239ccfba4024124bfc20a68f64b668a9bf4
himanshi0812 <himanshineema31@gmail.com>
2021-05-31 15:18:34 +0530

    add react file

---
+++ React/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function Car() {
+  return (
+    <div className="App">
+     <header className="App-header">
+   
+        <p>
+          <h1>Hello World</h1>
+        </p>
+           </header>
+    </div>
+  );
+}
+
+export default Car;
