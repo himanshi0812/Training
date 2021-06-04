0dfbef962cbf0410506b318fc51bbd4d34814ad2
himanshi0812 <himanshineema31@gmail.com>
2021-06-04 13:38:24 +0530

    Useparams Example

---
+++ React/urlparameter.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  BrowserRouter as Router,Switch, Route,Link,useParams} from "react-router-dom";
+
+export default function Params() {
+  return (
+    <Router>
+      <div>
+        <h2>Accounts</h2>
+
+        <ul>
+          <li>
+            <Link to="/netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/zillow-group">Zillow Group</Link>
+          </li>
+</ul>
+        <Switch>
+          <Route path="/:id" children={<Child />} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Child() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>ID: {id}</h3>
+    </div>
+  );
+}
