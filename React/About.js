6d8fb895016045e22a0a6c190d246aa205490a63
himanshi0812 <himanshineema31@gmail.com>
2021-06-04 12:15:40 +0530

    Nested Route and navigation using

--- React/About.js
+++ React/About.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Link,Route, Switch } from 'react-router-dom';
 import './Menu.css';
 
-const About = () =>{
+function About({match,location,histroy}){
+    // console.log(match);
+    // console.log(location);
+    // console.log(histroy);
     return(
       <div className = "About">
         <h1>WELCOME TO ABOUT PAGE</h1>
         <p>HII !!!</p>
+        <ul>
+        <li><Link to = {`${match.path}/Services`}>Services</Link></li>
+        <li> <Link to ={`${match.path}/Pricing`}>Pricing</Link></li>
+      </ul>
+      <Switch>
+      <Route exact path ={`${match.path}/Services`} component = {Servies} />
+      </Switch>
       </div>
     )
      
+  }
+  function Servies(){
+      return(
+          <div>
+              <ul>
+                  <li>WebDevlopment</li>
+                  <li>Web Designing</li>
+              </ul>
+          </div>
+      )
   }
   export default About;
