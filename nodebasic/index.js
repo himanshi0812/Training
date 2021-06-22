92b9c0da37ace96a3780e991878f356693ee34cf
himanshi0812 <himanshineema31@gmail.com>
2021-06-22 16:20:23 +0530

    file base module

--- nodebasic/index.js
+++ nodebasic/index.js
@@ -1,7 +1,8 @@
 // console.log('hello world');
-const square ={
-    area : (a) => (a*a)
-}
+// const square ={
+//     area : (a) => (a*a)
+// }
+const square = require('./square.js');
 
 const calsquare=(a)=>{
     console.log(`${a} and ` +square.area(a));
---
+++ nodebasic/square.js
@@ -0,0 +1,5 @@
+// exports.area = (a) => (a*a);
+
+const area =(a)=>(a*a)
+
+module.exports.area =area;
